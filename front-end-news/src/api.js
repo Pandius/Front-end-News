99cce9df34a61c72a0ac4b414695bf76b4d4d4ff
Pawel <pandius@poczta.fm>
2019-08-28 13:45:55 +0100

    added addVote,getadrticlebyId, postcomment, delete comment,getArticle comments

--- front-end-news/src/api.js
+++ front-end-news/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const BASE_URL = 'https://ncnewsbedos.herokuapp.com/api'
 
-export const getArticles = async (topic, sort_by, order, p) => {
+export const getArticles = async ({ topic, sort_by, order, p }) => {
     const { data } = await axios.get(`${BASE_URL}/articles`, {
         params: {
             topic,
@@ -11,4 +11,29 @@ export const getArticles = async (topic, sort_by, order, p) => {
         }
     })
     return data
+}
+
+
+export const addVote = async (type, id, inc_votes) => {
+    const { data } = await axios.patch(`${BASE_URL}/${type}/${id}`, { inc_votes: inc_votes })
+    return data
+}
+
+export const getArticleById = async article_id => {
+    const { data } = await axios.get(`${BASE_URL}/articles/${article_id}`)
+    return data.article
+}
+
+export const postComment = async (article_id, newComment) => {
+    const { data } = await axios.post(`${BASE_URL}/articles/${article_id}/comments`, newComment)
+    return data.comment
+}
+
+export const deleteCommentById = async comment_id => {
+    return axios.delete(`${BASE_URL}/comments/${comment_id}`)
+}
+
+export const getArticleComments = async article_id => {
+    const { data } = await axios.get(`${BASE_URL}/articles/${article_id}/comments`)
+    return data.comments
 }
