8132392d68574f02c5e7f4858dfdbbe6b592af54
Pawel <pandius@poczta.fm>
2019-08-30 09:42:21 +0100

    fix comments typo

--- front-end-news/src/components/CommentCard.jsx
+++ front-end-news/src/components/CommentCard.jsx
@@ -4,12 +4,11 @@ import Voter from './Voter'
 
 const CommentCard = (props) => {
     const { author, votes, created_at, comment_id, body } = props.comments
-
     return (
         <li key={comment_id}>
             <p>{body}</p>
             <p>Author: {author}</p>
-            <Voter votes={votes} id={comment_id} type='comment' />
+            <Voter votes={votes} id={comment_id} type='comments' />
             {<p>Added: {dateFormat(created_at)}</p>}
             {(props.username === author ?
                 <button onClick={() => props.handleDelete(comment_id)}>Delete Comment</button> : '')}
