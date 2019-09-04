8445d9e1cfecc2a485d2e3aa64d415f45f5cd85c
Pawel <pandius@poczta.fm>
2019-09-04 08:24:00 +0100

    added styling

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
 import northcoders from '../pictures/northcoders.png'
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -7,49 +7,49 @@ import { Link } from '@reach/router'
 
 
 
-class Navigation extends Component {
-    render() {
-        return (
-            <Navbar
-                className="navbar"
-                collapseOnSelect
-                expand="lg"
-                bg="light"
-                variant="light"
-                fixed="top"
-            >
-                <Link to='/'>
-                    <Navbar.Brand >
-                        <img className="brandImg" src={northcoders} alt="northcoders logo." />
-                    </Navbar.Brand>
-                </Link>
+const Navigation = (props) => {
 
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Link className="nav-link" to="/">Home   </Link>
-                        <Link to="/articles" className="nav-link"> Articles</Link>
-                        <NavDropdown title="Topics" id="collasible-nav-dropdown">
-                            <Link to="/articles/topic/coding" > Coding</Link>
-                            <NavDropdown.Divider />
+    return (
+        <Navbar
+            className="navbar"
+            collapseOnSelect
+            expand="lg"
+            bg="light"
+            variant="light"
+            fixed="top"
+        >
+            <Link to='/'>
+                <Navbar.Brand >
+                    <img className="brandImg" src={northcoders} alt="northcoders logo." />
+                </Navbar.Brand>
+            </Link>
 
-                            <Link to="/articles/topic/football">Football </Link>
-                            <NavDropdown.Divider />
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                    <Link className="nav-link" to="/">Home   </Link>
+                    <Link to="/articles" className="nav-link"> Articles</Link>
+                    <NavDropdown title="Topics" id="collasible-nav-dropdown">
+                        <Link to="/articles/topic/coding" > Coding</Link>
+                        <NavDropdown.Divider />
 
-                            <Link to="/articles/topic/cooking">Cooking</Link>
-                            <NavDropdown.Divider />
-                            <Link to="/"> Show all topics
+                        <Link to="/articles/topic/football">Football </Link>
+                        <NavDropdown.Divider />
+
+                        <Link to="/articles/topic/cooking">Cooking</Link>
+                        <NavDropdown.Divider />
+                        <Link to="/"> Show all topics
 
                             </Link>
-                        </NavDropdown>
-                    </Nav>
-                    <Nav className="user">
-                        User Logged in: <br /> {this.props.userLoggedIn}
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar >
-        );
-    }
+                    </NavDropdown>
+                </Nav>
+                <Nav className="user">
+                    User Logged in: <br /> {props.userLoggedIn}
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar >
+    );
 }
+
 
 export default Navigation;
